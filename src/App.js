3f11a45f7e5d2bb0c5904159e3294bd0de90b389
skkhinchi <khinchi.skumar@gmail.com>
2023-12-01 23:36:01 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Counter } from './Home/Counter';
+import { Header } from './Home/Header';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello Sumit</h1>
+     <Header/>
     <Counter/>
     </div>
   );
